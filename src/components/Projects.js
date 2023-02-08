f3f4c6b0b3ede67a07b4a38f782d44e886d01470
monysary <sary.mony@gmail.com>
2023-02-08 14:09:45 -0800

    added more content to projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,18 +3,45 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LanguageIcon from '@mui/icons-material/Language';
-import petPals from '../images/pet-pals.jpg'
+import petPals from '../images/pet-pals.jpg';
+import bitsPleaseChat from '../images/bits-please-chat.jpg';
+import weatherDashboard from '../images/weather-dashboard.jpg';
+import techBlog from '../images/tech-blog.jpg';
 
 function Projects() {
     const projects = [
         {
             name: 'Pet Pals',
-            description: 'A social media platform for pet owners to connect with other owners and chat with each other about their pets and schedule play dates!',
+            description: 'A social media platform for pet owners to connect and chat with each other about their pets and schedule play dates!',
             tech: ['React', 'Material UI', 'Node', 'Express', 'MongoDB', 'GraphQL', 'Socket.io'],
             url: 'https://hidden-headland-00556.herokuapp.com/',
             github: 'https://github.com/BryantTrinh/Pet-Pals-A-Social-Media-App-for-Pets',
             image: petPals
-        }
+        },
+        {
+            name: 'Bits Please Chat',
+            description: 'An online instant message app where users communicate using only emojis!',
+            tech: ['Handlebars', 'Tailwind', 'Node', 'Express', 'MySQL', 'Socket.io'],
+            url: 'https://damp-river-11459.herokuapp.com/',
+            github: 'https://github.com/monysary/Bits-Please-Chat',
+            image: bitsPleaseChat
+        },
+        {
+            name: 'The Weather Dashboard',
+            description: 'An online instant message app where users communicate using only emojis!',
+            tech: ['HTML', 'Bootstrap', 'Javascript', 'Web API'],
+            url: 'https://monysary.github.io/Weather-Dashboard/',
+            github: 'https://github.com/monysary/Weather-Dashboard',
+            image: weatherDashboard
+        },
+        {
+            name: 'The Tech Blog',
+            description: 'A blog website built using the CRUD operation for posting, reading, commenting, updating, and deleting user blog posts! Users must be logged in to see posts.',
+            tech: ['Handlebars', 'Bootstrap', 'Node', 'Express', 'MySQL'],
+            url: 'https://agile-headland-11573.herokuapp.com/',
+            github: 'https://github.com/monysary/Tech-Blog',
+            image: techBlog
+        },
     ]
 
     function ProjectCard({ name, description, tech, url, github, image }) {
@@ -26,7 +53,7 @@ function Projects() {
                         image={image}
                         title="green iguana"
                     />
-                    <CardContent>
+                    <CardContent sx={{ height: 190 }}>
                         <Typography gutterBottom variant="h5" component="div" textAlign='center'>
                             {name}
                         </Typography>
@@ -61,7 +88,7 @@ function Projects() {
                 <Typography fontSize='40px' component='h3' marginX='20px' sx={{ '@media (max-width: 600px)': { fontSize: '40px' } }}>Projects</Typography>
                 <ArrowForwardIosIcon sx={{ color: '#C83649', fontSize: '40px' }} />
             </Grid>
-            <Grid container>
+            <Grid container justifyContent='center' gap={2}>
                 {projects.map(project => <ProjectCard
                     name={project.name}
                     description={project.description}
